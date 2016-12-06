dfc00fd9501b4f4df4107de12fd4a786998b4b23
gillie97 <gilliano_herkul@live.nl>
2016-12-06 11:01:15 +0100

    Fixed CRUD for schools

--- resources/views/school/edit.blade.php
+++ resources/views/school/edit.blade.php
@@ -15,6 +15,9 @@
 		  			<input type="text" class="form-control" name="address_number" value="{{ $school->address_number }}" maxlength="10" />
 		  		</div>
 		  		<div class="form-group">
+		  			<input type="text" class="form-control" name="city" value="{{ $school->city }}" maxlength="35" required />
+		  		</div>
+		  		<div class="form-group">
 		  			<input type="text" class="form-control" name="zip_code" value="{{ $school->zip_code }}" maxlength="9" />
 		  		</div>
 		  		<div class="form-group">
@@ -24,7 +27,7 @@
 		  			<input type="email" class="form-control" name="email" value="{{ $school->email }}" maxlength="120" />
 		  		</div>
 		  		<div class="form-group">
-		  			<input type="text" class="form-control" name="city" value="{{ $school->city }}" maxlength="35" required />
+		  			<textarea name="description" class="form-control" placeholder="Beschrijving" rows="4">{{ $school->description }}</textarea>
 		  		</div>
 
 			  	<div class="form-group">
