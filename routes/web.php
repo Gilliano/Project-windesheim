cfe87a98d70280945d5310962558ec673355e76d
gillie97 <gilliano_herkul@live.nl>
2016-12-09 12:29:58 +0100

    Deleted duplicated routes

--- routes/web.php
+++ routes/web.php
@@ -32,12 +32,3 @@ Route::post('/persons/{id}', 'PersonsController@update');
 
 Route::get('/charts', 'ChartsController@index');
 Route::get('/json/charts/{functionName}', 'JSONController@decide');
-
-Route::get('/home', 'HomeController@index');
-Route::get('/schools', 'SchoolController@getSchools');
-Route::get('/schools/add', 'SchoolController@addSchool');
-Route::post('/schools/add', 'SchoolController@saveSchool');
-Route::get('/schools/{school}/edit', 'SchoolController@editSchool');
-Route::get('/schools/{school}/delete', 'SchoolController@deleteSchool');
-Route::get('/schools/{school}/restore', 'SchoolController@restoreSchool');
-Route::patch('/schools/{school}', 'SchoolController@updateSchool');
